bdaa975746fa43be1925ba85b0edde5f1930a427
Den4iks <lol4ik99@gmail.com>
2014-03-21 20:13:54 +0200

    Verstka

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 $(function (){
    $.getJSON('Dishes.json').success(function(data) {
     viewModel ={
-   		dishes: ko.observableArray([data])
+   		dishes: ko.observableArray(data)
    	}
-   	
+    ko.applyBindings(viewModel);
 });
 })
